00e76da31d6a7c7706397930d1f4edcb13f40df1
Harith Onigemo <onigemotosin@gmail.com>
2020-12-02 08:42:10 +0100

    Added Read more button

--- Assets/app.js
+++ Assets/app.js
@@ -1,6 +1,7 @@
 let resultTitle = document.getElementById('resultTitle');
 let resultBody = document.getElementById('resultBody');
-let resultBody2 = document.getElementById('resultBody2')
+let resultBody2 = document.getElementById('resultBody2');
+let link = document.querySelector('.link');
 
 async function apiCall(){
     let queryTerm = document.getElementById('searchInput').value;
@@ -17,5 +18,9 @@ async function apiCall(){
     resultTitle.innerText =  data.query.search[0].title;
     resultBody.innerHTML = "1. " + data.query.search[0].snippet;
     resultBody2.innerHTML = "2. " + data.query.search[1].snippet;
+    link.href = "https://en.wikipedia.org/wiki/" + queryTerm;
+    link.innerText = "Read More";
+
+    //Extra Styling
     document.querySelector('.resultContent').style.backgroundColor = "White";
 }
